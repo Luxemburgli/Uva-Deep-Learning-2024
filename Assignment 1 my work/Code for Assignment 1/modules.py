317b1355131de2a4453b40ac730b494ddd29bc16
Yipu Li (Lux) <148314069+Luxemburgli@users.noreply.github.com>
2025-01-09 23:44:16 +0100

    Add files via upload

---
+++ Assignment 1 my work/Code for Assignment 1/modules.py
@@ -0,0 +1,383 @@
+################################################################################
+# MIT License
+#
+# Copyright (c) 2024 University of Amsterdam
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Deep Learning Course (UvA) | Fall 2024
+# Date Created: 2024-10-28
+################################################################################
+"""
+This module implements various modules of the network.
+You should fill in code into indicated sections.
+"""
+import numpy as np
+
+
+class LinearModule(object):
+    """
+    Linear module. Applies a linear transformation to the input data.
+    """
+
+    def __init__(self, in_features, out_features, input_layer=False):
+        """
+        Initializes the parameters of the module.
+
+        Args:
+          in_features: size of each input sample
+          out_features: size of each output sample
+          input_layer: boolean, True if this is the first layer after the input, else False.
+
+        TODO:
+        Initialize weight parameters using Kaiming initialization.
+        Initialize biases with zeros.
+        Hint: the input_layer argument might be needed for the initialization
+
+        Also, initialize gradients with zeros.
+        """
+
+        # Note: For the sake of this assignment, please store the parameters
+        # and gradients in this format, otherwise some unit tests might fail.
+
+        self.params = {'weight': None, 'bias': None} # Model parameters
+        self.grads = {'weight': None, 'bias': None} # Gradients
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        scale = 2 / in_features
+
+        self.params['weight'] = np.random.normal(loc = 0.0, scale = scale,size = (out_features, in_features))
+
+        self.params['bias'] = 0
+
+        self.grads['weight'] = 0
+
+        self.grads['bias'] = 0
+
+        self.input_layer = input_layer
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+    def forward(self, x):
+        """
+        Forward pass.
+
+        Args:
+          x: input to the module
+        Returns:
+          out: output of the module
+
+        TODO:
+        Implement forward pass of the module.
+
+        Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        if self.input_layer == False:
+            out = x @ self.params['weight'].T + self.params['bias']
+
+            self.data = x
+        
+        else:
+            out = x
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
+
+    def backward(self, dout):
+        """
+        Backward pass.
+
+        Args:
+          dout: gradients of the previous module
+        Returns:
+          dx: gradients with respect to the input of the module
+
+        TODO:
+        Implement backward pass of the module. Store gradient of the loss with respect to
+        layer parameters in self.grads['weight'] and self.grads['bias'].
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        if self.input_layer == False:
+            x = self.data
+
+            dx = dout @ self.params['weight']
+
+            self.grads['weight'] = dout.T @ x
+
+            self.grads['bias'] = np.ones(np.shape(dout)[0]) @ dout
+        
+        else:
+            dx = 0
+ 
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+        return dx
+
+    def clear_cache(self):
+        """
+        Remove any saved tensors for the backward pass.
+        Used to clean-up model from any remaining input data when we want to save it.
+
+        TODO:
+        Set any caches you have to None.
+        """
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        self.data = None
+        pass
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+
+class ELUModule(object):
+    """
+    ELU activation module.
+    """
+
+    def __init__(self, alpha):
+        self.alpha = alpha
+
+    def forward(self, x):
+        """
+        Forward pass.
+
+        Args:
+          x: input to the module
+        Returns:
+          out: output of the module
+
+        TODO:
+        Implement forward pass of the module.
+
+        Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        self.data = x
+
+        out = np.where(x <= 0, self.alpha * (np.exp(x) - 1.0), x)
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
+
+    def backward(self, dout):
+        """
+        Backward pass.
+        Args:
+          dout: gradients of the previous module
+        Returns:
+          dx: gradients with respect to the input of the module
+
+        TODO:
+        Implement backward pass of the module.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        act_derivative = self.data
+
+        act_derivative = np.where(self.data <= 0, self.alpha * np.exp(self.data), np.ones((np.shape(self.data))))
+
+        dx = dout * act_derivative
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+        return dx
+
+    def clear_cache(self):
+        """
+        Remove any saved tensors for the backward pass.
+        Used to clean-up model from any remaining input data when we want to save it.
+
+        TODO:
+        Set any caches you have to None.
+        """
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        self.data = None
+        pass
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+
+class SoftMaxModule(object):
+    """
+    Softmax activation module.
+    """
+
+    def forward(self, x):
+        """
+        Forward pass.
+        Args:
+          x: input to the module
+        Returns:
+          out: output of the module
+
+        TODO:
+        Implement forward pass of the module.
+        To stabilize computation you should use the so-called Max Trick - https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
+
+        Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        max_data = np.max(x, axis = 1)
+
+        divider = np.sum(np.exp(x - max_data[:,np.newaxis]), axis = 1)
+
+        out = np.exp(x - max_data[:,np.newaxis]) / divider[:,np.newaxis]
+
+        self.out = out
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
+
+    def backward(self, dout):
+        """
+        Backward pass.
+        Args:
+          dout: gradients of the previous modul
+        Returns:
+          dx: gradients with respect to the input of the module
+
+        TODO:
+        Implement backward pass of the module.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        y = self.out
+
+        dimension = np.shape(y)[1]
+
+        dx = y * (dout - (dout * y) @ np.ones((dimension, dimension)))
+
+        
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return dx
+
+    def clear_cache(self):
+        """
+        Remove any saved tensors for the backward pass.
+        Used to clean-up model from any remaining input data when we want to save it.
+
+        TODO:
+        Set any caches you have to None.
+        """
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        self.out = None
+        
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+
+class CrossEntropyModule(object):
+    """
+    Cross entropy loss module.
+    """
+
+    def forward(self, x, y):
+        """
+        Forward pass.
+        Args:
+          x: input to the module
+          y: labels of the input
+        Returns:
+          out: cross entropy loss
+
+        TODO:
+        Implement forward pass of the module.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        y_onehot = (y[:,np.newaxis] == np.arange(np.shape(x)[1]))
+
+        out = np.trace(- np.log(x) @ y_onehot.T) / np.shape(x)[0]
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
+
+    def backward(self, x, y):
+        """
+        Backward pass.
+        Args:
+          x: input to the module
+          y: labels of the input
+        Returns:
+          dx: gradient of the loss with the respect to the input x.
+
+        TODO:
+        Implement backward pass of the module.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        y_onehot = (y[:,np.newaxis] == np.arange(np.shape(x)[1]))
+
+        dx = - (y_onehot / (x * np.shape(x)[0]))
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return dx
---
+++ Assignment 1 my work/Code for Assignment 1/train_mlp_pytorch.py
@@ -0,0 +1,251 @@
+################################################################################
+# MIT License
+#
+# Copyright (c) 2024 University of Amsterdam
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Deep Learning Course (UvA) | Fall 2024
+# Date Created: 2024-10-28
+################################################################################
+"""
+This module implements training and evaluation of a multi-layer perceptron in PyTorch.
+You should fill in code into indicated sections.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import numpy as np
+import os
+from copy import deepcopy
+from tqdm.auto import tqdm
+from mlp_pytorch import MLP
+import cifar10_utils
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+def accuracy(predictions, targets):
+    """
+    Computes the prediction accuracy, i.e. the average of correct predictions
+    of the network.
+
+    Args:
+      predictions: 2D float array of size [batch_size, n_classes], predictions of the model (logits)
+      labels: 2D int array of size [batch_size, n_classes]
+              with one-hot encoding. Ground truth labels for
+              each sample in the batch
+    Returns:
+      accuracy: scalar float, the accuracy of predictions,
+                i.e. the average correct predictions over the whole batch
+
+    TODO:
+    Implement accuracy computation.
+    """
+
+    #######################
+    # PUT YOUR CODE HERE  #
+    #######################
+
+    inflexible_prediction = torch.argmax(predictions, dim = 1)
+    #target_vector = torch.argmax(targets, dim = 1)
+    accuracy = (torch.sum(inflexible_prediction == targets)) / len(targets)
+
+    #######################
+    # END OF YOUR CODE    #
+    #######################
+
+    return accuracy
+
+
+def evaluate_model(model, data_loader):
+    """
+    Performs the evaluation of the MLP model on a given dataset.
+
+    Args:
+      model: An instance of 'MLP', the model to evaluate.
+      data_loader: The data loader of the dataset to evaluate.
+    Returns:
+      avg_accuracy: scalar float, the average accuracy of the model on the dataset.
+
+    TODO:
+    Implement evaluation of the MLP model on a given dataset.
+
+    Hint: make sure to return the average accuracy of the whole dataset,
+          independent of batch sizes (not all batches might be the same size).
+    """
+
+    #######################
+    # PUT YOUR CODE HERE  #
+    #######################
+    data_size = 0
+    sum_accuracy = 0
+
+    for batch in data_loader:
+      batch_reshape = batch[0].view(batch[0].shape[0], 3072)
+      flexible_prediction = model.forward(batch_reshape)
+      sum_accuracy += batch[0].shape[0] * accuracy(flexible_prediction, batch[1])
+      data_size += batch[0].shape[0]
+
+    avg_accuracy = sum_accuracy / data_size
+
+    #######################
+    # END OF YOUR CODE    #
+    #######################
+
+    return avg_accuracy
+
+
+def train(hidden_dims, lr, use_batch_norm, batch_size, epochs, seed, data_dir):
+    """
+    Performs a full training cycle of MLP model.
+
+    Args:
+      hidden_dims: A list of ints, specificying the hidden dimensionalities to use in the MLP.
+      lr: Learning rate of the SGD to apply.
+      use_batch_norm: If True, adds batch normalization layer into the network.
+      batch_size: Minibatch size for the data loaders.
+      epochs: Number of training epochs to perform.
+      seed: Seed to use for reproducible results.
+      data_dir: Directory where to store/find the CIFAR10 dataset.
+    Returns:
+      model: An instance of 'MLP', the trained model that performed best on the validation set.
+      val_accuracies: A list of scalar floats, containing the accuracies of the model on the
+                      validation set per epoch (element 0 - performance after epoch 1)
+      test_accuracy: scalar float, average accuracy on the test dataset of the model that
+                     performed best on the validation.
+      logging_dict: An arbitrary object containing logging information. This is for you to
+                    decide what to put in here.
+
+    TODO:
+    - Implement the training of the MLP model.
+    - Evaluate your model on the whole validation set each epoch.
+    - After finishing training, evaluate your model that performed best on the validation set,
+      on the whole test dataset.
+    - Integrate _all_ input arguments of this function in your training. You are allowed to add
+      additional input argument if you assign it a default value that represents the plain training
+      (e.g. '..., new_param=False')
+
+    Hint: you can save your best model by deepcopy-ing it.
+    """
+
+    # Set the random seeds for reproducibility
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():  # GPU operation have separate seed
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.determinstic = True
+        torch.backends.cudnn.benchmark = False
+
+    # Set default device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Loading the dataset
+    cifar10 = cifar10_utils.get_cifar10(data_dir)
+    cifar10_loader = cifar10_utils.get_dataloader(cifar10, batch_size=batch_size,
+                                                  return_numpy=False)
+
+    #######################
+    # PUT YOUR CODE HERE  #
+    #######################
+    train = cifar10_loader["train"] 
+    valid = cifar10_loader["validation"]
+    test = cifar10_loader["test"]
+    ## Load the data, these are all iterables.
+
+    # TODO: Initialize model and loss module
+    model = MLP(batch_size, hidden_dims, 10, use_batch_norm)
+    best_model, best_val_accu = model, 0
+    loss_module = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr)
+    # TODO: Training loop including validation
+    # TODO: Do optimization with the simple SGD optimizer
+    val_accuracies = []
+    loss_epoch = np.arange(epochs)
+    for i in range(epochs):
+      loss_epoch[i] = 0
+      for batch in train:
+          batch_reshape = batch[0].view(128, 3072)
+          x = model.forward(batch_reshape)
+          loss = loss_module(x, batch[1])
+          loss_without_grad = loss.detach()
+          loss_epoch[i] += loss_without_grad
+          ## Forwarding
+          ## Here I let the loss of an epoch be the sum of all the loss of the each batch. But actually I'm not sure whether this makes sense since the model is still being updated.
+
+          optimizer.zero_grad()
+          loss.backward()
+          optimizer.step()
+          ## Backwarding and Optimizing
+
+      
+
+      accu = evaluate_model(model, valid)
+      if accu > best_val_accu:
+          best_model = deepcopy(model)
+          best_val_accu = accu
+
+      val_accuracies.append(accu)
+
+    # TODO: Test best model
+    test_accuracy = evaluate_model(best_model, test)
+    # TODO: Add any information you might want to save for plotting
+    logging_dict = loss_epoch
+    #######################
+    # END OF YOUR CODE    #
+    #######################
+
+    return model, val_accuracies, test_accuracy, logging_dict
+
+
+if __name__ == '__main__':
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+
+    # Model hyperparameters
+    parser.add_argument('--hidden_dims', default=[500], type=int, nargs='+',
+                        help='Hidden dimensionalities to use inside the network. To specify multiple, use " " to separate them. Example: "256 128"')
+    parser.add_argument('--use_batch_norm', default = True, action='store_true',
+                        help='Use this option to add Batch Normalization layers to the MLP.')
+
+    # Optimizer hyperparameters
+    parser.add_argument('--lr', default=0.1, type=float,
+                        help='Learning rate to use')
+    parser.add_argument('--batch_size', default=128, type=int,
+                        help='Minibatch size')
+
+    # Other hyperparameters
+    parser.add_argument('--epochs', default=15, type=int,
+                        help='Max number of epochs')
+    parser.add_argument('--seed', default=42, type=int,
+                        help='Seed to use for reproducing results')
+    parser.add_argument('--data_dir', default='data/', type=str,
+                        help='Data directory where to store/find the CIFAR10 dataset.')
+
+    args = parser.parse_args()
+    kwargs = vars(args)
+
+    model, val_accuracies, test_accuracy, logging_dict = train(**kwargs)
+    # Feel free to add any additional functions, such as plotting of the loss curve here
+
+    #### I plot here
+
+    import matplotlib.pyplot as plt
+    plt.subplot(1,2,1)
+    plt.plot(np.arange(len(val_accuracies)),val_accuracies, label = "validation accuracy")
+    plt.legend()
+    plt.subplot(1,2,2)
+    plt.plot(np.arange(len(logging_dict)),logging_dict, label = "loss")
+    plt.legend()
+    plt.show()
+    print(test_accuracy)
